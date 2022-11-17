602121b584d836d6f8c9779d079c65505199858c
kadir inan <102403844+GoGoAga@users.noreply.github.com>
2022-11-17 23:27:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// CommentBox is an ES module
 import { CommentBox } from "./commentBox.js";
-
-// Set the current user data into the session storage.
 sessionStorage.setItem("currentUser", "Kadir");
-
-// Define the CommentBox class as a custom element aka Web Component
-// Custom HTML tag for this element is set as 'comment-box'
 window.customElements.define("comment-box", CommentBox);
